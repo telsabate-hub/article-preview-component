683ce6041072378bae1ed0743e4fb32aca14c8e7
Tel <telsabate.business@gmail.com>
2025-02-20 21:41:43 +0800

    Added event handler for mobile and fixed some styles for mobile.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-const button = document.querySelector( "#shareBtn" );
+const button = document.querySelector( "#btn1" );
 const tooltip = document.querySelector( ".social-media-links" );
+const shareLinksDiv = document.querySelector( ".mobile-share-links" );
+const authorDiv = document.querySelector( ".author" );
+const articleDetails = document.querySelector( ".article-details" );
 
 button.addEventListener( "click", function(){
-    tooltip.removeAttribute( "hidden" );
+    const isMobile = ( window.screen.width <= 820 );
+
+    if( isMobile ){
+        shareLinksDiv.style.display = "block";
+        authorDiv.style.display = "none";
+        articleDetails.classList.toggle( "share-links-visible" );
+    } else {
+        tooltip.removeAttribute( "hidden" );
+        articleDetails.classList.toggle( "share-links-visible" );
+    }
 });
 
 document.addEventListener('click', function(event) {
